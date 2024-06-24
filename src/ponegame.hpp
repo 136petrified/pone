a240e794e35da5a784759e78cf8525cf4c749d2f
136petrified <95456211+136petrified@users.noreply.github.com>
2024-06-23 21:54:26 -0700

    Update ponegame.cpp + ponegui.hpp

---
+++ src/ponegame.hpp
@@ -0,0 +1,10 @@
+#ifndef PONE_GAME_HPP
+#define PONE_GAME_HPP
+
+class Game {
+public:
+    Game();
+    ~Game();
+}
+
+#endif // PONE_GAME_HPP
---
+++ src/ponegui.hpp
@@ -0,0 +1,10 @@
+#ifndef PONE_GUI_HPP
+#define PONE_GUI_HPP
+
+class GUI {
+public:
+    GUI();
+    ~GUI();
+};
+
+#endif // PONE_GUI_HPP
