0ebfdadf441acf0074ece6c92f670ac74ebc09ee
136petrified <95456211+136petrified@users.noreply.github.com>
2024-06-25 15:48:29 -0700

    Update ponetile.hpp

--- src/ponetile.hpp
+++ src/ponetile.hpp
@@ -1,10 +1,19 @@
 #ifndef PONE_TILE_HPP
 #define PONE_TILE_HPP
 
+#include <string>
+
 class Tile {
 public:
     Tile();
     ~Tile();
+private:
+    struct Attributes {
+        int x, y;           // coordinates of the tile
+        std::string color;
+        std::string type;   // what is the type of this tile?
+        bool isCursor;      // is the cursor on this tile?
+    };
 };
 
 #endif // PONE_TILE_HPP
