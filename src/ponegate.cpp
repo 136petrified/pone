8df0ecb0711c624591828d36f03a70c0c75c9a0c
136petrified <95456211+136petrified@users.noreply.github.com>
2024-08-08 21:16:03 -0700

    Define functions for ponegate

--- src/ponegate.cpp
+++ src/ponegate.cpp
@@ -1,3 +1,20 @@
 /*  Created: JUN 29 2024
 *   Modified: AUG 8 2024
 */
+
+#include "ponegate.hpp"
+
+// Gate constructors
+// ---------------------------------------------
+Gate::Gate() : gate{std::pair<Tile*, Tile*>{nullptr, nullptr}}
+{}
+
+Gate::Gate(Tile *t1, Tile *t2) : gate{std::pair<Tile*, Tile*>{t1, t2}}
+{}
+
+// Gate getter/setter functions
+// ---------------------------------------------
+void Gate::setTile1(Tile *t1) {
+    gate.first = t1;
+}
+
--- src/ponegate.hpp
+++ src/ponegate.hpp
@@ -7,11 +7,29 @@
 
 class Gate {
 public:
+    // Gate constructors
+    // ---------------------------------------------
+    Gate();
+    Gate(Tile *t1, Tile *t2);
+
+    // Gate getter/setter functions
+    // ---------------------------------------------
+    void setTile1(Tile *t1);
+    Tile *getTile1() const;
+
+    void setTile2(Tile *t2);
+    Tile *getTile2() const;
+
+    void setGate(std::pair<Tile*, Tile*> gate);
+    std::pair<Tile*, Tile*> getGate() const;
+
+    // Gate destructor
+    ~Gate();
+
 private:
     // Gate attributes
     // ---------------------------------------------
-    std::pair<Tile*, Tile*> gate;
-    
+    std::pair<Tile*, Tile*> gate; // Is a gate between two adjacent tiles.
 };
 
 #endif // __PONE_GATE_HPP
