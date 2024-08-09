32d3d8ccc3a204970714a8f4f213e734e5b551b7
136petrified <95456211+136petrified@users.noreply.github.com>
2024-08-08 20:51:11 -0700

    Update ponegate

--- src/ponegate.hpp
+++ src/ponegate.hpp
@@ -1,10 +1,17 @@
 #ifndef __PONE_GATE_HPP
 #define __PONE_GATE_HPP
 
+#include <utility>
+
+#include "ponetile.hpp"
+
 class Gate {
 public:
 private:
     // Gate attributes
+    // ---------------------------------------------
+    std::pair<Tile*, Tile*> gate;
+    
 };
 
 #endif // __PONE_GATE_HPP
