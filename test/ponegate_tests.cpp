c2784b5e1ca5aa500a256088964c242ce43ea95e
136petrified <95456211+136petrified@users.noreply.github.com>
2025-06-13 15:50:26 -0700

    Test ponegate

--- test/ponegate_tests.cpp
+++ test/ponegate_tests.cpp
@@ -9,13 +9,34 @@ TEST(ponegate, Constructor) {
     EXPECT_EQ(tilePair.first, nullptr);
     EXPECT_EQ(tilePair.second, nullptr);
 
-    Gate g2{Tile{2, 3, "none", "empty"}, Tile{4, 5, "none", "empty"}};
+    Tile t1 = Tile{2, 3, "none", "empty"};
+    Tile t2 = Tile{4, 5, "none", "empty"};
 
-    Tile *t1 = g2.getTile1();
-    EXPECT_EQ(t1->getX(), 2);
-    EXPECT_EQ(t1->getY(), 3);
+    Gate g2{&t1, &t2};
 
-    Tile *t2 = g2.getTile2();
-    EXPECT_EQ(t2->getX(), 4);
-    EXPECT_EQ(t2->getY(), 5);
+    Tile *target1 = g2.getTile1();
+    EXPECT_EQ(target1->getX(), 2);
+    EXPECT_EQ(target1->getY(), 3);
+
+    Tile *target2 = g2.getTile2();
+    EXPECT_EQ(target2->getX(), 4);
+    EXPECT_EQ(target2->getY(), 5);
+}
+
+TEST(ponegate, ID) {
+    Gate g;
+    g.setID(64);
+    EXPECT_EQ(g.getID(), 64);
+}
+
+TEST(ponegate, Color) {
+    Gate g;
+    g.setColor("green");
+    EXPECT_EQ(g.getColor(), "green");
+}
+
+TEST(ponegate, Name) {
+    Gate g;
+    g.setName("g1");
+    EXPECT_EQ(g.getName(), "g1");
 }
