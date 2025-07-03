325d65179687bb53904072d927296b5c6ecf9e16
136petrified <95456211+136petrified@users.noreply.github.com>
2025-07-02 21:58:48 -0700

    Fix postorder traversal

--- src/poneavl.hpp
+++ src/poneavl.hpp
@@ -229,8 +229,8 @@ void AVLNode<T>::postorder(AVLNode<T> *root, std::vector<T> &vec) {
         return;
     }
 
-    inorder(root->left, vec);
-    inorder(root->right, vec);
+    postorder(root->left, vec);
+    postorder(root->right, vec);
     vec.push_back(root->data);
 }
 
