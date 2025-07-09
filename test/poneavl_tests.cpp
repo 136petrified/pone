fd543fe3c51433fb8168d6c0dc13bd3028a0593d
136petrified <95456211+136petrified@users.noreply.github.com>
2025-07-08 23:04:04 -0700

    Test AVL

--- test/poneavl_tests.cpp
+++ test/poneavl_tests.cpp
@@ -1,11 +1,16 @@
 #include <gtest/gtest.h>
 
 #include "poneavl.hpp"
+#include "ponetile.hpp"
 
 TEST(AVL, Constructor) {
     AVL<int> tree;
 
     EXPECT_TRUE(tree.empty());
+
+    AVL<Tile> tileTree;
+
+    EXPECT_TRUE(tileTree.empty());
 }
 
 TEST(AVL, Insert) {
@@ -22,5 +27,24 @@ TEST(AVL, Insert) {
     std::vector<int> expected = {-29, -17, -5, 20, 43, 58, 92};
     EXPECT_EQ(expected, tree.inorder());
 
-    tree.printInorder();
+    AVL<Tile> tileTree;
+
+    Tile t1{5, 4, "purple", "collision"};
+    Tile t2{1, 9, "green", "empty"};
+    Tile t3{13, 1, "none", "tswitch"};
+    Tile t4{2, 30, "red", "gswitch"};
+    Tile t5{5, 0, "none", "key"};
+    Tile t6{3, 3, "gray", "collision"};
+    Tile t7{46, 4, "none", "goal"};
+
+    tileTree.insert(t1);
+    tileTree.insert(t2);
+    tileTree.insert(t3);
+    tileTree.insert(t4);
+    tileTree.insert(t5);
+    tileTree.insert(t6);
+    tileTree.insert(t7);
+
+    std::vector<Tile> tileExpected = {t2, t4, t6, t5, t1, t3, t7};
+    EXPECT_EQ(tileExpected, tileTree.inorder());
 }
