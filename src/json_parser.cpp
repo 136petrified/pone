663a71bcd09c3f5f41a9f8ee8fdc3dbe8db8066e
136petrified <95456211+136petrified@users.noreply.github.com>
2025-07-17 22:00:16 -0700

    Create static functions for json_parser

--- src/json_parser.cpp
+++ src/json_parser.cpp
@@ -1,3 +1,88 @@
 /*   Created:  07-17-2025
  *   Modified: 07-17-2025
  */
+
+#include "json_parser.hpp"
+
+#include <cctype>
+#include <stdexcept>
+
+namespace JSON {
+Parser::Parser() {}
+
+Parser::Parser(const Tokenizer &tokenizer)
+    : m_stop{false}, m_tc{1}, m_tokens{tokenizer.getTokens()} {
+    m_currentToken = (m_tokens.empty()) ? "" : m_tokens[0];
+}
+
+void Parser::expr() {}
+
+void Parser::next() {
+    try {
+        m_currentToken = m_tokens.at(m_tc++);
+    } catch (std::out_of_range &) {
+        m_stop = true;
+    }
+}
+
+void Parser::parse() {
+    while (!m_stop) {
+    }
+}
+
+void Parser::symbol() {
+    if (m_currentToken == "(") {
+        next();  // Consume '(' token;
+        while (m_currentToken != ")") {
+            paren();
+        }
+    } else if (m_currentToken == "[") {
+        next();
+        while (m_currentToken != "]") {
+            squareBracket();
+        }
+    } else if (m_currentToken == "{") {
+        next();
+        while (m_currentToken == "}") {
+            curlyBracket();
+        }
+    } else if (m_currentToken == ":") {
+        next();
+    } else if (m_currentToken == ",") {
+        next();
+    }
+}
+
+bool Parser::isAlnum(std::string &s) {
+    for (char &c : s) {
+        if (!std::isalnum(c)) return false;
+    }
+
+    return true;
+}
+
+bool Parser::isAlpha(std::string &s) {
+    for (char &c : s) {
+        if (!std::isalpha(c)) return false;
+    }
+
+    return true;
+}
+
+bool Parser::isDigit(std::string &s) {
+    for (char &c : s) {
+        if (!std::isalpha(c)) return false;
+    }
+
+    return true;
+}
+
+bool Parser::isSymbol(std::string &s) {
+    for (char &c : s) {
+        if (!std::ispunct(c)) return false;
+    }
+
+    return true;
+}
+
+}  // namespace JSON
--- src/json_parser.hpp
+++ src/json_parser.hpp
@@ -1,3 +1,40 @@
 /*   Created:  07-17-2025
  *   Modified: 07-17-2025
  */
+
+#ifndef PONE_JSON_PARSER_HPP
+#define PONE_JSON_PARSER_HPP
+
+#include <stack>
+
+#include "json_tokenizer.hpp"
+
+namespace JSON {
+class Parser {
+   public:
+    Parser();
+    Parser(const Tokenizer &tokenizer);
+    void curlyBracket();
+    void expr();
+    void next();
+    void paren();
+    void parse();
+    void squareBracket();
+    void symbol();
+
+    static bool isAlnum(std::string &s);
+    static bool isAlpha(std::string &s);
+    static bool isDigit(std::string &s);
+    static bool isSymbol(std::string &s);
+    ~Parser();
+
+   private:
+    bool m_stop;
+    int m_tc;
+    std::string m_currentToken;
+    std::vector<std::string> m_tokens;
+    std::stack<std::string> m_parenStack;
+};
+}  // namespace JSON
+
+#endif  // PONE_JSON_PARSER_HPP
