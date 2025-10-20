182c430cb8fc90a556ed5791a52a913b58ea625e
136petrified <95456211+136petrified@users.noreply.github.com>
2025-10-19 20:56:02 -0700

    Add tests

--- test/game/avl_test.cpp
+++ test/game/avl_test.cpp
@@ -13,7 +13,6 @@ TEST(avl_test, Insert) {
     AVL<int> tree;
 
     tree.insert(5);
-    tree.insert(8);
     tree.insert(7);
     tree.insert(2);
     tree.insert(1);
@@ -22,8 +21,24 @@ TEST(avl_test, Insert) {
     tree.insert(9);
     tree.insert(4);
     tree.insert(3);
+    tree.insert(6);
 
-    std::vector<int> solution = {0, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::vector<int> solution = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     EXPECT_EQ(solution, tree.preorder());
+}
+
+TEST(avl_test, Less) {
+    AVLNode<int> node{-4};
+    EXPECT_TRUE(node.less(-4, 7));
+}
+
+TEST(avl_test, Equal) {
+    AVLNode<int> node{-4};
+    EXPECT_TRUE(node.equal(-4, -4));
+}
+
+TEST(avl_test, Greater) {
+    AVLNode<int> node{-4};
+    EXPECT_TRUE(node.greater(-4, -7));
 }
