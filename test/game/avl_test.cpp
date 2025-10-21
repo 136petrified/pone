b855927bd2ccc4caf6e51bca794a956ba4f1ae45
136petrified <95456211+136petrified@users.noreply.github.com>
2025-10-20 17:30:33 -0700

    Add AVL tests

--- test/game/avl_test.cpp
+++ test/game/avl_test.cpp
@@ -28,6 +28,57 @@ TEST(avl_test, Insert) {
     EXPECT_EQ(solution, tree.inorder());
 }
 
+TEST(avl_test, Find) {
+    AVL<int> tree;
+
+    tree.insert(5);
+    tree.insert(7);
+    tree.insert(2);
+    tree.insert(1);
+    tree.insert(0);
+    tree.insert(8);
+    tree.insert(9);
+    tree.insert(4);
+    tree.insert(3);
+    tree.insert(6);
+
+    for (int i = 0; i < 10; ++i)
+        EXPECT_NE(tree.find(i), nullptr);
+}
+
+TEST(avl_test, Remove) {
+    AVL<int> tree;
+
+    tree.insert(5);
+    tree.insert(7);
+    tree.insert(2);
+    tree.insert(1);
+    tree.insert(0);
+    tree.insert(8);
+    tree.insert(9);
+    tree.insert(4);
+    tree.insert(3);
+    tree.insert(6);
+
+    tree.remove(0);
+    tree.remove(9);
+    tree.remove(1);
+    tree.remove(5);
+    tree.remove(4);
+    tree.remove(7);
+
+    std::vector<int> solution = {2, 3, 6, 8};
+
+    EXPECT_EQ(solution, tree.inorder());
+
+    tree.remove(2);
+    tree.remove(8);
+    tree.remove(3);
+    tree.remove(6);
+
+    EXPECT_TRUE(tree.empty());
+}
+
 TEST(avl_test, Less) {
     AVLNode<int> node{-4};
     EXPECT_TRUE(node.less(-4, 7));
